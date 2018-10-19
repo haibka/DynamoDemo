b45b91be8431fa0ff966a5137450b13bc9d98570
ライ・ヴァン・ハイ <vlai@netprotections.co.jp>
2018-10-19 10:01:34 +0900

    firt commit

---
+++ src/main/java/com/amazonaws/samples/MoviesCreateTable.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.amazonaws.samples;
+
+/**
+ * @author v.lai
+ *
+ */
+import java.util.Arrays;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+
+public class MoviesCreateTable {
+
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+            .build();
+       //エンドポイントを設定した後はDYNAMODBにテーブルを作成ことをしめする。
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName = "Movies";
+
+        try {
+            System.out.println("Attempting to create table; please wait...");
+            Table table = dynamoDB.createTable(tableName,
+                Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition パーティションキーを追加する
+                                                                          // key
+                    new KeySchemaElement("title", KeyType.RANGE)), // Sort key    ソートキーを追加する
+                Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N), // Yearの価値タイプを追加
+                    new AttributeDefinition("title", ScalarAttributeType.S)), // Titleの価値タイプを追加
+                new ProvisionedThroughput(10L, 10L));
+            table.waitForActive();
+            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to create table: ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
